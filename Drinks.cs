d2c956ae5bb05c5bdd7e6063ace88673d429ed42
Jing Tan <jtan1993@gmail.com>
2021-11-20 23:30:24 +0000

    Update Drinks.cs

--- Drinks.cs
+++ Drinks.cs
@@ -14,18 +14,40 @@ namespace FlyweightPattern
     }
 
     public class Espresso: IDrinkFlyweight{
-        public string Name { get {return "Espresso";} }
+        private string _name;
+        public string Name { get {return _name;} }
+        private IEnumerable<string> _ingredients;
+
+        // Set intrinsic values using constructor
+        public Espresso{
+            _name = "Espresso";
+            _ingredients = new List<string>(){
+                "Coffee Beans",
+                "Hot Water"
+            };
+        }
         
         public void Serve(string size){
-            Console.WriteLine($"Size: ");
+            Console.WriteLine($"Order: {size} {_name} with {string.Join(", ", _ingredients)} coming up!");
         }
     }
 
     public class MangoLassi: IDrinkFlyweight{
-        public string Name { get {return "Mango Lassie";} }
-        
+        private string _name;
+        public string Name { get {return _name;} }
+        private IEnumerable<string> _ingredients;
+
+        public MangoLassi{
+            _name = "Mango Lassi";
+            _ingredients = new List<string>(){
+                "Mango",
+                "Yoghurt",
+                "Vanilla Extract"
+            };
+        }
+
         public void Serve(string size){
-            Console.WriteLine($"Size: ");
+            Console.WriteLine($"Order: {size} {_name} with {string.Join(", ", _ingredients)} coming up!");
         }
     }    
 }
